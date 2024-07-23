0b8f42715be6f90c461354ea90fe5a56733facfc
phiyada555 <653070310137@npu.ac.th>
2024-07-23 16:05:48 +0700

    stat new contract

---
+++ tips.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract tips {
+    address owner;
+    constructor(){
+        owner = msg.sender;
+
+    }
+
+    //1.ใส่เงินเข้าไปในกระปุก
+    //2.ดูจำนวนเงินในกระปุก
+    //3.เพิ่มคนที่จะได้รับทิป
+    //4.ลบคนที่จะได้รับทิป
+    //5.ดูคนที่จะได้รับทิป
+    //6.จ่ายเงิน
+}
