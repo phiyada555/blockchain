c97419bd2baeeb884f5c40ec4b5875ecc6ce1426
phiyada555 <653070310137@npu.ac.th>
2024-07-30 15:24:58 +0700

    สร้างฟังชั่นเติมเงิน/ดูยอดเงิน

--- tips.sol
+++ tips.sol
@@ -9,8 +9,21 @@ contract tips {
     }
 
     //1.ใส่เงินเข้าไปในกระปุก
+
+    function addtips() payable public {
+
+    }
+
     //2.ดูจำนวนเงินในกระปุก
+
+    function viewtips() public view returns(uint){
+        return address(this).balance;
+    }
+
     //3.เพิ่มคนที่จะได้รับทิป
+
+    
+
     //4.ลบคนที่จะได้รับทิป
     //5.ดูคนที่จะได้รับทิป
     //6.จ่ายเงิน
