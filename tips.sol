49d0aafaf34aca0d6f4590dfee41f15d11ffbd32
phiyada555 <653070310137@npu.ac.th>
2024-08-13 15:27:57 +0700

    add waitress

--- tips.sol
+++ tips.sol
@@ -2,7 +2,11 @@
 pragma solidity 0.8.26;
 
 contract tips {
+
     address owner;
+    Waitress[] waitress;
+    
+
     constructor(){
         owner = msg.sender;
 
@@ -20,11 +24,40 @@ contract tips {
         return address(this).balance;
     }
 
-    //3.เพิ่มคนที่จะได้รับทิป
+    //3.1 สร้างโครงสร้างของพนักงาน
 
+    struct Waitress{
+        address payable walletAddress;
+        string name;
+    }
+
+    //3.2 เพิ่มพนักงาน
+
+    function addwaitress(address payable walletAddress,string memory name) public {
+        bool waitressExist = false;
+
+        if(waitress.length >=1){
+        for(uint i=0; i<waitress.length; i++){
+           if(waitress[i].walletAddress == walletAddress){
+            waitressExist = true;
+           }
+        }
+    }
+
+    if(waitressExist==false){
+        waitress.push(Waitress(walletAddress,name));
+        
+        }
+    }
+
+    //4.ลบพนักงานที่จะได้รับทิป
     
+    //5.ดูพนักงานที่จะได้รับทิป
 
-    //4.ลบคนที่จะได้รับทิป
-    //5.ดูคนที่จะได้รับทิป
+    function viewWaitress() public view returns(Waitress[] memory) {
+        return waitress;
+    }
+
     //6.จ่ายเงิน
+
 }
